a9748e850c85d2d7597bd64ae0397e9305bfd4ea
Karol Martinez C <>
2021-01-10 23:54:12 -0500

    inicio y final proyecto penitenciario con el tiempo

--- src/penitenciaria/Penitenciaria.java
+++ src/penitenciaria/Penitenciaria.java
@@ -18,24 +18,41 @@ public class Penitenciaria {
      */
     public static void main(String[] args) {
        Scanner leer= new Scanner(System.in);
-       int N, c=1, MESES, cif=0;//cif contador de if
+       int N, MESES, cif=0;//cif contador de if
+       
        String nombre;
         System.out.println("DIGITE EL NUMERO DE INTERNOS EN EL PENITENCIARIO: ");
         N=leer.nextInt();
+        
+        String [][]reporte = new String [N][2]; // DEFINIR MATRIZ 
  
-       while (c<=N){
-           System.out.println("DIGITE EL NOMBRE DEL INTERNO #"+c);
+       for (int c=0;c<N;c++){
+           System.out.println("DIGITE EL NOMBRE DEL INTERNO #"+(c+1));
            nombre=leer.next();
            System.out.println("DIGITE EL NUMERO DE CONDENA EN MESES");
            MESES=leer.nextInt();
            
            if(MESES<36){
            cif++;
+           reporte [c][0] = nombre;
+           reporte [c][1] = String.valueOf(MESES); //convierte el int a un string / MESES lo va a convertir en letras/ string
            }
            
            
-          c++;
        }
+           
+          System.out.println("EL REPORTE CON LOS INTERNIS CON UNA CONDENA MENOR A 36 MESES"); 
+          
+       for(int i=0;i<N;i++){ //i : contaodr del for
+           
+           if( (reporte[i][0] != null) && (reporte[i][1] != null)){
+           System.out.println("----------------------------------------------");  
+           System.out.println("Nombre del recluso: "+reporte[i][0]+" numero #"+(i+1));
+           System.out.println("Condena en meses: "+reporte[i][1]);
+               
+       }       
+       }
+       System.out.println("---------------------------------------------");
         System.out.println("EL NUMERO DE INTERNOS CON MENOS DE 36 MESES DE CONDENA ES DE: "+cif);
         
     }
